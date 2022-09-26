ae9f4c1ef24cf18f644be90c3eb9421e95923731
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-09-26 17:50:22 +0200

    Updated main to show the usage of head and title tag

--- app/src/main/kotlin/html/dls/with/kotlin/App.kt
+++ app/src/main/kotlin/html/dls/with/kotlin/App.kt
@@ -4,6 +4,7 @@ fun main() {
     with(HtmlDsl) { // this: HtmlDsl
         val htmlCode: String = html {
             // it is like be in HTML class now
+            head { title {  } }
         }.render()
         print(htmlCode)
     }
