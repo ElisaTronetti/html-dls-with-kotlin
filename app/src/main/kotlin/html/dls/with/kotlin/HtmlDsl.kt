237b0fc81f8c19044bad05061472d7a2fe2610ee
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-09-26 17:33:02 +0200

    Added tag interface and abstract class to create a common render behavior

--- app/src/main/kotlin/html/dls/with/kotlin/HtmlDsl.kt
+++ app/src/main/kotlin/html/dls/with/kotlin/HtmlDsl.kt
@@ -2,17 +2,26 @@ package html.dls.with.kotlin
 
 object HtmlDsl {
 
-    class HTML() {
-        val name: String = "html"
+    interface Tag {
+        val name: String
+        fun render(): String
     }
 
-    class Head() {
-        val name: String = "head"
+    abstract class AbstractTag(override val name: String) : Tag {
+        private val indent: String = " "
+
+        override fun render(): String =
+            """
+            $indent<$name>
+            $indent</$name>
+            """.trimIndent()
     }
 
-    class Title(){
-        val name: String = "title"
-    }
+    class HTML() : AbstractTag("html")
+
+    class Head() : AbstractTag("head")
+
+    class Title() : AbstractTag("title")
 
     fun html(init: HTML.() -> Unit): HTML = HTML().apply(init)
 
