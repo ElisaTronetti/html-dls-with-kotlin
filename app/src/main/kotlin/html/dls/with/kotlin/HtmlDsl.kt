46e86cb1d8c41497af4cec2d7fb038db30f93a85
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-09-26 15:47:17 +0200

    Added class for the html tag wanted for the DSL

---
+++ app/src/main/kotlin/html/dls/with/kotlin/HtmlDsl.kt
@@ -0,0 +1,17 @@
+package html.dls.with.kotlin
+
+object HtmlDsl {
+
+    class HTML() {
+        val name: String = "html"
+    }
+
+    class head() {
+        val name: String = "head"
+    }
+
+    class title(){
+        val name: String = "title"
+    }
+
+}
