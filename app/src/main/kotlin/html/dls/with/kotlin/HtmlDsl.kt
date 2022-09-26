46ef62154a910e058b1755fc52f5e42c6792a461
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-09-26 18:31:26 +0200

    Added function to add a child tag to a tag

--- app/src/main/kotlin/html/dls/with/kotlin/HtmlDsl.kt
+++ app/src/main/kotlin/html/dls/with/kotlin/HtmlDsl.kt
@@ -4,25 +4,32 @@ object HtmlDsl {
 
     interface Tag {
         val name: String
+        val children: List<Tag> // a tag might have children that have to be rendered
         fun render(): String
+        fun addChild(child: Tag): Unit
     }
 
     abstract class AbstractTag(override val name: String) : Tag {
         private val indent: String = " "
+        override var children: List<Tag> = emptyList()
 
         override fun render(): String =
             """
             $indent<$name>
             $indent</$name>
             """.trimIndent()
+
+        override fun addChild(child: Tag) {
+            children = children + child
+        }
     }
 
     class HTML() : AbstractTag("html") {
-        fun head(conf: Head.() -> Unit): Head = Head().apply(conf)
+        fun head(conf: Head.() -> Unit) = addChild(Head().apply(conf))
     }
 
     class Head() : AbstractTag("head") {
-        fun title(conf: Title.() -> Unit): Title = Title().apply(conf)
+        fun title(conf: Title.() -> Unit) = Title().apply(conf)
     }
 
     class Title() : AbstractTag("title")
